84dc731f342f8f0e201cf0beaa714374d106f5cc
Lost1nC0d3 <L0st1nC0d3@users.noreply.github.com>
2021-07-21 09:35:22 +0200

    Update buildMakeDB.js

--- src/data-access/buildMakeDB.js
+++ src/data-access/buildMakeDB.js
@@ -7,26 +7,21 @@ export default function buildMakeDB({ filePath, fs }) {
 			throw new Error('A valid filesystem object handler must be provided')
 		}
 
-		let jsonContent = []
-
 		const content = () => {
-			if (jsonContent.length === 0) {
-				const rawData = fs.readFileSync(filePath)
-				jsonContent = JSON.parse(rawData)
-			}
-			
-			return jsonContent
+			const rawData = fs.readFileSync(filePath)
+			return JSON.parse(rawData)
 		}
 
 		const edit = (todo) => {
 			if (!todo) {
 				throw new Error('The todo item must have been provided')
 			}
-			const oldOne = content().filter(p => p.id === todo.id)[0]
+			const elements = content()
+			const oldOne = elements.filter(p => p.id === todo.id)[0]
 			oldOne.date = todo.date
 			oldOne.title = todo.title
-			const index = content().findIndex(p => p.id === todo.id)
-			const updatedContent = Object.assign([], content())
+			const index = elements.findIndex(p => p.id === todo.id)
+			const updatedContent = Object.assign([], elements)
 			updatedContent.splice(index, 1)
 			updatedContent.push(oldOne)
 			const newContent = JSON.stringify(updatedContent)
@@ -34,7 +29,6 @@ export default function buildMakeDB({ filePath, fs }) {
 				fs.writeFileSync(filePath, newContent)
 				return todo
 			} catch (error) {
-				console.log(error)
 				return null
 			}
 		}
@@ -50,7 +44,6 @@ export default function buildMakeDB({ filePath, fs }) {
 				fs.writeFileSync(filePath, newRawData)
 				return newTodo
 			} catch (error) {
-				console.log(error)
 				return null
 			}
 		}
@@ -59,8 +52,9 @@ export default function buildMakeDB({ filePath, fs }) {
 			if (!todoId) {
 				throw new Error('An item id must be provided')
 			}
-			const index = content().findIndex(p => p.id === todoId)
-			const updatedContent = Object.assign([], content())
+			const elements = content()
+			const index = elements.findIndex(p => p.id === todoId)
+			const updatedContent = Object.assign([], elements)
 			updatedContent.splice(index, 1)
 			const newContent = JSON.stringify(updatedContent)
 			try {
@@ -69,7 +63,6 @@ export default function buildMakeDB({ filePath, fs }) {
 					message: `Deleted todo with ID ${todoId}`
 				})
 			} catch (error) {
-				console.log(error)
 				return null
 			}
 		}
